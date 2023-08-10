ca1825774041b388d33444ae84b9dbda31cf8f44
Adam Martinu <1337programmering.c@gmail.com>
2023-08-10 11:10:20 +0200

    Added constructor that accepts Throwable cause

--- src/main/java/dk/martinu/opti/img/spi/InvalidImageException.java
+++ src/main/java/dk/martinu/opti/img/spi/InvalidImageException.java
@@ -6,6 +6,10 @@ public class InvalidImageException extends Exception {
         super(msg);
     }
 
+    public InvalidImageException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
     public InvalidImageException(String msg, Object... args) {
         this(String.format(msg, args));
     }
