553be604146d770eaf3bb2272bfb9441065af54c
Adam Martinu <1337programmering.c@gmail.com>
2024-02-07 20:40:32 +0100

    Fixed CRC value check and possible invalid types

--- src/main/java/dk/martinu/opti/img/spi/ChunkReader.java
+++ src/main/java/dk/martinu/opti/img/spi/ChunkReader.java
@@ -8,13 +8,22 @@ import java.util.zip.CRC32;
 
 public class ChunkReader {
 
+    private static boolean isTypeValid(int type) {
+        for (int i = 0; i < 4; i++, type >>>= 8) {
+            int b = type & 0xFF;
+            if ((b < 'A' || b > 'Z') && (b < 'a' || b > 'z')) {
+                return false;
+            }
+        }
+        return true;
+    }
     protected final ReadableByteChannel input;
 
     public ChunkReader(ReadableByteChannel input) {
         this.input = Objects.requireNonNull(input, "input is null");
     }
 
-    public Chunk getChunk() throws IOException {
+    public Chunk getChunk() throws IOException, ImageDataException {
         // reusable int buffer
         final ByteBuffer bInt = ByteBuffer.allocate(4);
         // used to compute CRC value of chunk
@@ -34,6 +43,9 @@ public class ChunkReader {
             throw new IOException("missing chunk type");
         }
         int type = bInt.flip().getInt();
+        if (!isTypeValid(type)) {
+            throw new ImageDataException("chunk type is invalid {" + Chunk.typeToString(type) + "}");
+        }
         crc32.update(bInt.flip());
 
         /* DATA */
@@ -49,8 +61,8 @@ public class ChunkReader {
             throw new IOException("missing chunk CRC");
         }
         int crc = bInt.flip().getInt();
-        if (crc != crc32.getValue()) {
-            throw new IOException("invalid CRC value, data is corrupt");
+        if (crc != (int) crc32.getValue()) {
+            throw new IOException("invalid CRC value for chunk {" + Chunk.typeToString(type) + "}, data is corrupt");
         }
 
         return new Chunk(type, data, crc);
