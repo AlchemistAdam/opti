ec86f71a7751603a02f84ff012a346a8cdced4df
Adam Martinu <1337programmering.c@gmail.com>
2024-04-17 01:13:00 +0200

    Refactoring

--- src/main/java/dk/martinu/opti/img/png/NullMethod.java
+++ src/main/java/dk/martinu/opti/img/png/NullMethod.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");  you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package dk.martinu.opti.img.png;
 
 import dk.martinu.opti.img.spi.ImageDataException;
@@ -11,9 +27,12 @@ final class NullMethod implements InterlaceMethod {
     @Override
     public byte[] getPngSamples(int width, int height, int bitDepth, ColorType colorType, FilterMethod filterMethod,
             byte[] filterData, byte[] palette, byte[] transparency, byte[] background) throws ImageDataException {
+
+        byte[] samples = filterMethod.reconstruct(bitDepth, colorType,
+                filterData, height, (int) Math.ceil(width * colorType.getComponentCount() * bitDepth / 8.0));
         // single reduced image containing the samples
-        ReducedImage img = new ReducedImage(width, height, filterMethod.revert(
-                filterData, height, (int) Math.ceil(width * colorType.getComponentCount() * bitDepth / 8.0)));
+        ReducedImage img = new ReducedImage(width, height, samples);
+
         // pixel setter for reduced image samples
         PixelSetter setter = colorType.getPixelSetter(bitDepth, img, palette, transparency, background);
 
